862fcf783ac1cc1fadee6a9325869903b048cbff
Dellannia Segreti <dellannia.segreti@mail.utoronto.ca>
2026-03-06 10:07:07 -0500

    finaba

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -44,6 +44,7 @@ mkdir user_logs
 mkdir event_logs 
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd data/raw
 cp "server*.log" data/processed/server_logs 
 
 # 6. Repeat the above step for user logs and event logs
