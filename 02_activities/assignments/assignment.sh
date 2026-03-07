7e2962cf5ab5190aeafff9fa15517c95a33be88e
Dellannia Segreti <dellannia.segreti@mail.utoronto.ca>
2026-03-07 17:34:49 -0500

    done4

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -54,7 +54,7 @@ rm data/raw/*ipaddr*
 rm data/processed/user_logs/*ipaddr* 
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls -R /data/processed > data/inventory.txt 
+find /data/processed -type f > data/inventory.txt 
 
 
 
